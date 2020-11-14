8835ddc06cf7453e53a2daab306ae859b8e31c7c
nagytommy76 <nagytommy76@gmail.com>
2020-11-14 20:14:09 +0100

    Work in progress: Creating crew skill tooltip

--- src/components/Vehicle/VehicleModalComponents/Crew.vue
+++ src/components/Vehicle/VehicleModalComponents/Crew.vue
@@ -1,27 +1,47 @@
 <template>
     <div class="crew-skills">
         <div class="crew-skills-item" v-for="(member,index) in crew" :key="index">
-            <!-- <h3>{{ member.roles[member.member_id] }}</h3> -->
-            <div class="tooltip">
-                <img :src="this.getCrewImages(index+1)" alt="Crew Member">
-                <div class="tooltip-text">
+            <h3>{{ member.roles[member.member_id] }}</h3>
+            <div class="crew-tooltip">
+                <img class="crew-image" :src="this.getCrewImages(index+1)" alt="Crew Member">
+                <div class="crew-tooltip-text">
                     <p v-for="(roles, roleIndex) in member.roles" :key="roleIndex">
                         {{roles}}
                     </p>
+                    <CrewSkills 
+                        :crewMemeberQualification="crewQualifications[member.member_id]"
+                    />
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import CrewSkills from './CrewSkills'
+import axios from 'axios'
+
 export default {
     name: 'Crew Members',
     props: {
         crew: Object,
         crewNation: String,
     },
-    created(){
-        this.getCrewImages(1)
+    components:{
+        CrewSkills,
+    },
+    data() {
+        return {
+            crewQualifications: {
+                commander: {},
+                gunner: {},
+                driver: {},
+                radioman: {},
+                loader: {},
+            },
+        }
+    },
+    created() {
+        this.getCrewQualifications()
     },
     methods:{
         // Le kell kérnem a bejövő crew-ból a crew qualifications-t
@@ -29,7 +49,15 @@ export default {
         // egy kattintással előjön egy valami és abban a crew qualifications jelenik meg
         getCrewImages(iteration){
             return `https://eu-wotp.wgcdn.co/static/5.73.0_022301/wotp_static/img/tankopedia_new/frontend/scss/tankopedia-detail/img/crew/${this.crewNation}-face-${iteration}.png`
-        }
+        },
+        async getCrewQualifications(){
+            for (const key of Object.keys(this.crewQualifications)) {
+                await axios.get(`https://api.worldoftanks.eu/wot/encyclopedia/crewskills/?application_id=1ebc47797ed02032c3c5489cbba60f6c&role=${key}`)
+                .then(skills => {
+                    this.crewQualifications[key] = skills.data.data
+                })
+            }
+        },
     },
 }
 </script>
---
+++ src/components/Vehicle/VehicleModalComponents/CrewSkills.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="crew-qualification" v-for="(skill, index) in crewMemeberQualification" :key="index">
+        <div class="crew-qualification-icon">
+            <div class="tooltip">
+                <img :src="skill.image_url.big_icon" alt="Skill Icon">            
+                <div class="tooltip-text">
+                    <p>{{ skill.description}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="crew-qualification-name">
+            <p>{{skill.name}}</p>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'Crew Skills',
+    props:{
+        crewMemeberQualification: Object,
+    }
+}
+</script>
